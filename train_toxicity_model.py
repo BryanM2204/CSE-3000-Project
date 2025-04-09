e59593b7e2f072ea81a7dc13e211227d25ccf13a
BryanM2204 <martinezbryan753@gmail.com>
2025-04-09 00:25:05 -0400

    updated the toxicity training script

--- train_toxicity_model.py
+++ train_toxicity_model.py
@@ -3,6 +3,9 @@ from bert_dataset import load_and_prepare_dataset
 
 dataset, target_labels, target2id, id2target = load_and_prepare_dataset("Dynamically Generated Hate Dataset v0.2.3.csv")
 
+dataset['train'] = dataset['train'].rename_column("label", "labels")
+dataset['test'] = dataset['test'].rename_column("label", "labels")
+
 model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
 
 training_args = TrainingArguments(
